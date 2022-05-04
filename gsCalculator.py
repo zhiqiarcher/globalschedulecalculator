3588d0131f3736e53d5e46bd4e19dc74885f30c3
zhiqiarcher <zhiqi.liu@foxmail.com>
2022-05-04 16:19:19 +0800

    change experiment

--- gsCalculator.py
+++ gsCalculator.py
@@ -4,6 +4,7 @@ import os,sys
 #this package calculate by layer
 #- no, by layer is not correct
 #make some change
+#memememmeme change
 
 def Calculate(NodeSet,PortSet,DAGSet):
     print('*********************************Start of cyclic DAG scheduling process********************************************************')
